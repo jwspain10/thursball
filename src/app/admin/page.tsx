ffb49de245e2e470cb4223321fe07ae19b385909
JW <joewellwood@gmail.com>
2026-06-07 17:34:28 +0200

    admin cache issue fix

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,7 @@
 import UserList from "./components/UserList";
 import { fetchAllUsers } from "./api/fetchAllUsers";
 
+export const dynamic = "force-dynamic";
 export default async function AdminPage() {
   const users = await fetchAllUsers();
   return <UserList users={users} />;
